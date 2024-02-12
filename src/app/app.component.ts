6a3757fe2159eb9a39214b862c96bac6297908b3
me@angelamiracle.dev <me@angelamiracle.dev>
2024-02-12 18:44:15 -0500

    Commiting Todo List Lab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Todo } from './app.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Todo_List';
+    // name will automatically be bound to the input with the [(ngModel)]="name"
+    task: string = '';
+  
+    completed: boolean = false;
+  
+    duration: number = 0;
+  
+    // defaults as an empty array!
+    todos: Todo[] = [
+    {task: "wash dishes", completed: false, duration: 30}, 
+    {task: "groceries", completed: true, duration: 1}, 
+    {task: "laundry", completed: false, duration: 2}
+  ];
 }
